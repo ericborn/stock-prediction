083572acba4c69aad46235d92d46f0c491d86dcc
Eric Born <eric.born85@gmail.com>
2019-11-12 11:46:35 -0800

    Finished KNN implementation

--- predictions.py
+++ predictions.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 Eric Born
-
+Utilizing various ML algorithms to predict stock prices
 """
 
-import os
+import os, sys
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -39,7 +39,7 @@ try:
 
 except Exception as e:
     print(e)
-    print('failed to read stock data for ticker: ', ticker)
+    sys.exit('failed to read stock data for ticker: ', ticker)
 
 # Create class column where red = 0 and green = 1
 df['class'] = df['label'].apply(lambda x: 1 if x =='green' else 0)
@@ -93,11 +93,11 @@ y_test = df_2018_reduced['label'].values
 
 # Scaler for training data
 scaler.fit(x_train)
-x_2017_train = scaler.transform(x_train)
+x_train_scaled = scaler.transform(x_train)
 
 # Scaler for test data
 scaler.fit(x_test)
-x_2018_test = scaler.transform(x_test)
+x_test_scaled  = scaler.transform(x_test)
 
 #####
 # KNN
@@ -120,10 +120,10 @@ try:
         knn_classifier = KNeighborsClassifier(n_neighbors = k)
        
         # Train the classifier
-        knn_classifier.fit(x_train, y_train)
+        knn_classifier.fit(x_train_scaled, y_train)
         
         # Perform predictions
-        pred_k = knn_classifier.predict(x_test)
+        pred_k = knn_classifier.predict(x_test_scaled)
         
         # Store error rate and accuracy for particular K value
         k_value.append(k)
@@ -131,7 +131,7 @@ try:
         accuracy.append(round(sum(pred_k == y_test) / len(pred_k) * 100, 2))
 except Exception as e:
     print(e)
-    print('failed to build the KNN classifier.')
+    sys.exit('failed to build the KNN classifier.')
 
 for i in range (0,5):
     print('The accuracy on 2017 data when K =', k_value[i], 'is:', accuracy[i])
@@ -145,33 +145,26 @@ plt.title('Accuracy vs. k for stock labels')
 plt.xlabel('Number of neighbors: k')
 plt.ylabel('Accuracy')
 
-# setup and test on 2018 data with k = 7
-# Create x test set for 2018
-x_test = df_2018_reduced[features].values
-y_2018_test = df_2018_reduced['label'].values
-
-# scaler for 2018 test data
-scaler.fit(x_test)
-x_2018_test = scaler.transform(x_test)
-
-# Create the classifier with neighbors set to 5
+# Create the classifier with neighbors set to 7
 knn_2018 = KNeighborsClassifier(n_neighbors = 7)
 
 # Train the classifier using all of 2017 data
 knn_2018.fit(x_train, y_train)
         
 # Perform predictions on 2018 data
-pred_2018 = knn_2018.predict(x_2018_test)
+knn_pred_2018 = knn_2018.predict(x_test_scaled)
 
 # Capture error and accuracy rates for 2018 predictions
-error_2018 = round(np.mean(pred_2018 != y_2018_test) * 100, 2)
-accuracy_2018 = round(sum(pred_2018 == y_2018_test) / len(pred_2018) * 100, 2)
+knn_error_2018 = round(np.mean(knn_pred_2018 != y_test) * 100, 2)
+knn_accuracy_2018 = round(sum(knn_pred_2018 == y_test) / 
+                          len(knn_pred_2018) * 100, 2)
 
-# accuracy is 83.13%
-print('\nThe accuracy on 2018 data when K = 5 is:', accuracy_2018, '%')
+# print accuracy
+print('\nThe accuracy on 2018 data when K = 7 is:', knn_accuracy_2018, '%')
 
 # Output the confusion matrix
-cm = confusion_matrix(y_2018_test, pred_2018)
+cm = confusion_matrix(y_test, knn_pred_2018)
+tn, fp, fn, tp  = confusion_matrix(y_test, knn_pred_2018).ravel()
 print('\nConfusion matrix for year 2 predictions:')
 print(cm, '\n')
 
@@ -191,12 +184,121 @@ plt.title('Confusion matrix', y=1.1)
 plt.ylabel('Actual label')
 plt.xlabel('Predicted label')
 
+# TPR/TNR rates
+print('The TPR is:', str(tp) + '/' + str(tp + fn) + ',',
+      round(recall_score(y_test, knn_pred_2018) * 100, 2),'%')
+print('The TNR is:', str(tn) + '/' + str(tn + fp) + ',',
+    round(tn / (tn + fp) * 100, 2),'%')
+
+# Implement a trading strategy based upon label predicitons 
+# Initialize wallet and shares to track current money and number of shares.
+knn_wallet = 100.00
+knn_shares = 0
+knn_profit = 0
+knn_worth = 0
+
+# stores adj_close values for the last day of each trading week
+adj_close = df_2018.groupby('td_week_number')['adj_close'].last()
+
+# stores open price for the first day of each trading week
+open_price = df_2018.groupby('td_week_number')['open'].first()
+
+# for loop that evaluates the dataset deciding when to buy/sell based
+# upon the prediction labels. 0 is a bad week, 1 is a good week
+try:
+    for i in range(0, len(knn_pred_2018)):
+        # Sell should occur on the last day of a green week at 
+        # the adjusted_close price. Since i is tracking the current
+        # trading week we need to minus 1 to get the adjusted close price
+        # from the previous trading week
+        if knn_pred_2018[i] == 0 and knn_shares > 0:
+            knn_wallet = round(knn_shares * adj_close[i - 1], 2)
+            shares = 0
+            
+        # Buy should occur on the first day of a green week at the open price
+        if knn_pred_2018[i] == 1 and knn_shares == 0: 
+            knn_shares = knn_wallet / open_price[i]
+            knn_wallet = 0            
+            
+except Exception as e:
+    print(e)
+    sys.exit('Failed to evaluate df_2018 labels')
+
+# set worth by multiplying stock price on final day by total shares
+knn_worth = round(shares * adj_close[52], 2)
+
+if knn_worth == 0:
+    knn_worth = knn_wallet
+    knn_profit = round(knn_wallet - 100.00, 2)
+else:
+    knn_profit = round(knn_worth - 100.00, 2)
+
+#####
+# Logistic Regression
+#####
+
+# Create a logistic classifier
+# set solver to avoid the warning
+log_reg_classifier = LogisticRegression(solver = 'liblinear')
+
+# Train the classifier on 2017 data
+log_reg_classifier.fit(x_2017_train, y_train)
+
+# Predict using 2018 feature data
+prediction = log_reg_classifier.predict(x_2018_test)
+
+# print coefficient and intercept
+# print(log_reg_classifier.coef_)
+# print(log_reg_classifier.intercept_)
+
+print('output the coefficients with feature names')
+coef = log_reg_classifier.coef_
+for p,c in zip(features,list(coef[0])):
+    print(p + '\t' + str(c))
+
+# 1)
+# what is the equation for logistic regression that your 
+# classifier found from year 1 data?
+print('\nThe equation for logistic regression found in year 1 data is:')
+print('y = 0.1621 + 2.0277*mu - 0.0168*sig')
+
+# 2)
+# what is the accuracy for year 2?
+# 79.245% accuracy
+accuracy = np.mean(prediction == y_test)
+print('\nThe accuracy for year 2 is:')
+print(round(accuracy * 100, 2), '%')
+
+# 3)
+# Output the confusion matrix
+cm = confusion_matrix(y_test, prediction)
+print('\nConfusion matrix for year 2 predictions:')
+print(cm, '\n')
+
+# Create confusion matrix heatmap
+# setup class names and tick marks
+class_names=[0,1]
+fig, ax = plt.subplots()
+tick_marks = np.arange(len(class_names))
+plt.xticks(tick_marks, class_names)
+plt.yticks(tick_marks, class_names)
+
+# Create heatmap and labels
+sns.heatmap(pd.DataFrame(cm), annot=True, cmap="BrBG" ,fmt='g')
+ax.xaxis.set_label_position("top")
+plt.tight_layout()
+plt.title('Confusion matrix', y=1.1)
+plt.ylabel('Actual label')
+plt.xlabel('Predicted label')
+
+# 4)
 # what is true positive rate (sensitivity or recall) and true
 # negative rate (specificity) for year 2?
-print('The specificity is: 20/29 = 0.69 = 69%')	
-print('The recall is: 23/24 =', 
-      round(recall_score(y_2018_test, pred_2018) * 100, 2),'%')
+print('The recall is: 22/24 =', 
+      round(recall_score(y_test, prediction) * 100, 2),'%')
+print('The specificity is: 20/29 = 68.97%')	
 
+# 5)
 # Implemented trading strategy based upon label predicitons vs
 # buy and hold strategy
 
@@ -214,17 +316,17 @@ open_price = df_2018.groupby('td_week_number')['open'].first()
 # for loop that evaluates the dataset deciding when to buy/sell based
 # upon the prediction labels. 0 is a bad week, 1 is a good week
 try:
-    for i in range(0, len(pred_2018)):
+    for i in range(0, len(prediction)):
         # Sell should occur on the last day of a green week at 
         # the adjusted_close price. Since i is tracking the current
         # trading week we need to minus 1 to get the adjusted close price
         # from the previous trading week
-        if pred_2018[i] == 0 and shares > 0:
+        if prediction[i] == 0 and shares > 0:
             wallet = round(shares * adj_close[i - 1], 2)
             shares = 0
             
         # Buy should occur on the first day of a green week at the open price
-        if pred_2018[i] == 1 and shares == 0: 
+        if prediction[i] == 1 and shares == 0: 
             shares = wallet / open_price[i]
             wallet = 0            
             
@@ -251,27 +353,10 @@ print('Total Cash: $', wallet, '\nTotal shares:', round(shares, 6),
       '\nWorth: $', worth)    
 print('This method would close the year at $', worth, 'a profit of $', profit)
 
-# Buy and hold
-# Initialize wallet and shares to track current money and number of shares.
-wallet = 100.00
-shares = 0
-profit = 0
-worth = 0
+######
+# Linear Regression
+######
 
-# Calculate shares, worth and profit
-shares = round(wallet / float(open_price[0]), 6)
-worth = round(shares * adj_close[52], 2)
-profit = round(worth - 100.00, 2)
-
-#Currently own 4.009623 shares 
-#Worth $ 141.70
-#Selling on the final day would result in $ 141.7 a profit of $ 41.7
-print('\n2018 buy and hold:','\nCurrently own', shares, 'shares',
-      '\nWorth','$',"%.2f"%round(worth, 2))
-print('Selling on the final day would result in $',"%.2f"%worth, 
-      'a profit of $', "%.2f"%profit)
-
-###########
 # This section of code calculates the regression for each day incrementing
 # through a window size from 5 to 30 days.
 # If the predicted close price for w+1 is greater than the close price for w,
@@ -712,3 +797,36 @@ plt.title('Total Trades 2017 vs. 2018')
 plt.xlabel('Year')
 plt.ylabel('Total Number of Trades')
 plt.show()
+
+#####
+# Setup for buy and hold strat
+#####
+
+# Buy and hold
+# Initialize wallet and shares to track current money and number of shares.
+bh_wallet = 100.00
+bh_shares = 0
+bh_profit = 0
+bh_worth = 0
+
+# Calculate shares, worth and profit
+shares = round(bh_wallet / float(open_price[0]), 6)
+bh_worth = round(shares * adj_close[52], 2)
+bh_profit = round(bh_worth - 100.00, 2)
+
+#####
+# Output profits from each algorithm
+#####
+
+# KNN profits
+print('\KNN Label Strategy:')
+print('Total Cash: $', knn_wallet, '\nTotal shares:', round(knn_shares, 6),
+      '\nWorth: $', knn_worth)    
+print('This method would close the year at $', knn_worth, 'a profit of $', 
+      knn_profit)
+
+# Buy and hold profits
+print('\n2018 buy and hold:','\nCurrently own', bh_shares, 'shares',
+      '\nWorth','$',"%.2f"%round(bh_worth, 2))
+print('Selling on the final day would result in $',"%.2f"%bh_worth, 
+      'a profit of $', "%.2f"%bh_profit)
